a14ccdcb24510e630e8f48c30d316f43fc05b95b
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2022-06-23 16:49:42 -0400

    Add derivations.nix

---
+++ derivations.nix
@@ -0,0 +1,27 @@
+# To install all the dependencies into the current user only, run:
+#
+#     $ nix-env -f derivations.nix --install '.*'
+#
+# You can check the installation:
+#
+#     $ nix-env --query --installed
+#     apache-maven-3.8.5
+#     curl-7.83.1
+#     delve-1.8.2
+#     dotnet-sdk-3.1.415
+#     gcc-wrapper-11.3.0
+#     ...
+#
+# To keep your install up-to date use --upgrade:
+#
+#     $ nix-env -f derivations.nix --upgrade '.*'
+#
+# See also:
+#
+#     man nix-env
+
+let
+  pkgs = import <nixpkgs> {};
+  derivs = import ./required-packages.nix {pkgs = pkgs;};
+
+in derivs ++ [pkgs.gnumake]

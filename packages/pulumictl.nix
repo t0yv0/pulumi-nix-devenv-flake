75d7a5fe782f3a1b48df2290ae284306f5a54d01
Anton Tayanovskyy <anton@pulumi.com>
2022-08-22 13:15:01 -0400

    Upgrade pulumictl

--- packages/pulumictl.nix
+++ packages/pulumictl.nix
@@ -2,12 +2,12 @@
 
 pkgs.buildGoModule rec {
   pname = "pulumictl";
-  version = "0.0.29";
+  version = "0.0.32";
   src = pkgs.fetchFromGitHub {
     owner = "pulumi";
     repo = "pulumictl";
     rev = "v${version}";
-    sha256 = "1cf0mbgqsbc7a39v5ndhaf55pbzlgqz5klzjcg6izyr006vg2d6s";
+    sha256 = "0q8rm8699i41v5dxr1ahyzggw6sj7rkwl1gbl4d3zji70nfh7789";
   };
   doCheck = false;
   vendorSha256 = "0szp7gibv8bh6jkca71mkqgww2m6fghxny30kdpz0v1snyf5zaf5";
